1429b65a28119426fbc21d1e1625bb77a7080497
Student apnaCollege <priyanshukumarbgs066@gmail.com>
2026-02-27 01:53:03 +0530

    Update Backend uri after deploy

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,7 @@ const app = express();
 // Security Middlewares
 app.use(helmet());
 app.use(cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://task-manager-inky-nu.vercel.app"],
     credentials: true
 }));
 
